4d410d85edb952f2a811fa2f01eee66304fabe58
victor muasya <83345578+muasya-victor@users.noreply.github.com>
2022-04-07 07:59:42 +0300

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken ('authtoken')->plainTextToken;
+
+        $response = [
+            'user'=> $user,
+            'token' => $token
+        ];
+
+        return response($response , 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string|'
+        ]);
+
+        // confirm email
+        $user = User::where('email', $fields['email'])->first();
+
+        // password confirm
+        if (!$user || Hash::check($fields['password'], $user->password))
+        {
+            return response (
+                [
+                    'message'=> 'invalid credentials'
+                ], 
+                401
+                );
+        }
+
+        $token = $user->createToken ('authtoken')->plainTextToken;
+
+        $response = [
+            'user'=> $user,
+            'token' => $token
+        ];
+
+        return response($response , 201);
+    }
+
+    public function logout(Request $request)
+    {
+        
+        $this->Auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'logged out'
+        ];
+    }
+}
